c3abd35ce452a3486003a0dcb2ce457458439e7e
Joachim Skjævestad <joachim@biketrial.no>
2019-07-21 15:45:37 +0200

    online

--- public/new.js
+++ public/new.js
@@ -1,8 +1,7 @@
-let w; 
+let w;
 let h;
-let size;
-let joinedPlayer=false;
-
+let gameSize = 1000;
+let joinedPlayer = false;
 
 let usernameText;
 let subBtn;
@@ -10,123 +9,204 @@ let subBtn2;
 
 let players = {};
 let data = {};
+let history = [];
+let apples = [];
 
 let userName;
-let userX=200;
-let userY=200;
+let userX = 200;
+let userY = 200;
+let userMass=50;
+let connectCounter=0;
 
 
 
 function setup() {
-    
+
     //CANVAS
-    w = 1920;
-    h = 1080;
-    size = windowWidth/w;
-    createCanvas(w,h);
-    scale(size);
-    console.log(size);
-   
-   
+    w = windowWidth;
+    h = windowHeight;
+    createCanvas(w, h);
+
     //STARTMENY
     usernameText = createInput('Name');
-    usernameText.position(windowWidth/2-100,200);
+    usernameText.position(w / 2 - 100, 200);
     usernameText.mousePressed(clearIn);
-    subBtn = createButton('OK');
-    subBtn.position((windowWidth/2)+60,200);
+    subBtn = createButton('JOIN');
+    subBtn.position((w / 2) + 60, 200);
     subBtn.id('subBtnId');
     subBtn.mousePressed(gameStart);
     subBtn2 = createButton('Show Info');
-    subBtn2.position(windowWidth-100,windowHeight-100);
+    subBtn2.position(w - 100, h - 100);
     subBtn2.mousePressed(viewData);
-  
+
 
 
     //DIS WORKED:
-    var person =[]; 
-    person['abc'] = {navn:"Ola Nordmann",alder: 12};
-    person['ppp'] = {navn:"Kari Nordmann",alder: 24};
+    var person = [];
+    person['abc'] = { navn: "Ola Nordmann", alder: 12 };
+    person['ppp'] = { navn: "Kari Nordmann", alder: 24 };
     var text = "";
     var x;
     for (x in person) {
-      text += person[x].alder;
+        text += person[x].alder;
     }
     console.log(text);
     // bare for å hjelpe altså
 }
 function draw() {
-    //blob(random(300),random(500));
-  
 
-    if(!joinedPlayer){
+    // LAST HISTORIEN UANSETT
+    socket.on('history', function (serverHistory) {
+
+        history = serverHistory.history;
+
+
+    });
+    socket.on('total players', function (num){
+        connectCounter=num.connectCounter;
+    
+        
+    });
+    socket.on('apples', function(serverApples){
+        for(var a = 0; a<serverApples.apples.length;a++){
+            apples[a] = new Apple;
+            apples[a].x = serverApples.apples[a].x;
+            apples[a].y = serverApples.apples[a].y;
+            apples[a].nr = a;
+            
+            
+        }
+        //console.log(apples);
+        // apples = serverApples.apples;
+    });
+    socket.on('move apple', function(serverApples){
+       // console.log(serverApples);
+            apples[serverApples.data.nr].x = serverApples.data.x;
+            apples[serverApples.data.nr].y = serverApples.data.y;
+        
+            
+        
+        //console.log(apples);
+        // apples = serverApples.apples;
+    });
+
+
+    if (!joinedPlayer) { //MENYEN
         background(255);
         textAlign(CENTER);
-        text("Velkommen til multiplayerspillet!", windowWidth/2,190);
-        
+        text("Velkommen til multiplayerspillet!", w / 2, 190);
+        fill(0);
 
-     }else{
-        background(214,12,124);
+
+        textAlign(LEFT);
+        text("NUMBER OF PLAYERS: " + connectCounter,100,85);
+        printHistory();
+
+    } else {  //SELVE SPILLET:
+        background(82, 178, 70);
+       
+
+       
+
+        //EMIT at du vil oppdatere, henter oppdatering og plasser blobsa på nytt
+        socket.on('update players', function (serverPlayers) {
+            players = serverPlayers;
+        });
+        for (var a =0; a<apples.length;a++) {
+            // console.log(players[x].id,socket.id);
+            apples[a].show();
+        }
+       
+        for (var x in players) {
+            // console.log(players[x].id,socket.id);
+            if (players[x].id != socket.id) {
+                blob(players[x].name, players[x].playerx, players[x].playery, players[x].mass);
+            }
+        }
+      
+        userX += (mouseX - userX) / 30;
+        userY += (mouseY - userY) / 30;
+
         fill(255);
-      sendInfo(); blob(userName, userX, userY);
-    
-    
-    
-   
-    //EMIT at du vil oppdatere, henter oppdatering og plasser blobsa på nytt
-    socket.emit('update players');
-    socket.on('update players', function (serverPlayers) {
-        players = serverPlayers;
-    });
-   
-    for (var x in players) {
-       // console.log(players[x].id,socket.id);
-        if(players[x].id!=socket.id){
-        blob(players[x].name, players[x].playerx, players[x].playery);
-    }
-    }
-   
+        textAlign(LEFT); //PRINT HISTORY
+        printHistory();
         
-        userX +=(mouseX - userX) / 30;
-        userY +=(mouseY - userY) /30;
 
-    
+        text("NUMBER OF PLAYERS: " + connectCounter,100,85);
 
-    
+        sendInfo();
+        blob(userName, userX, userY,userMass);
+
+
     }
 }
-function clearIn(){
-   if(usernameText.value() == "Name"){ usernameText.value("");}
+function clearIn() {
+    if (usernameText.value() == "Name") { usernameText.value(""); }
 }
-function viewData(){
+function viewData() {
     socket.emit('view data');
 }
+function printHistory() {
 
-function gameStart(){
-    joinedPlayer=true;
+    for (i = 0; i < 10; i++) {
+        if (history[i]) { text(history[i], 100, 100 + (15 * i)); }
+    }
+
+}
+
+function gameStart() {
+    joinedPlayer = true;
     userName = usernameText.value();
     usernameText.hide();
     subBtn.hide();
+    socket.emit('player joined', userName);
+    socket.emit('get apples');
 
 }
-function sendInfo(){
-    data = {id: socket.id, name: userName,playerx:userX,playery: userY};
+function sendInfo() {
+    data = { id: socket.id, name: userName, playerx: userX, playery: userY, mass: userMass};
     socket.emit('send player', data);
- 
+
 }
-function blob(dname,dx,dy){
+function blob(dname, dx, dy,mass) {
     fill(255);
-    ellipse(dx,dy,50,50);
+    ellipse(dx, dy, mass, mass);
     fill(0);
     textAlign(CENTER);
-    text(dname,dx,dy);
+    text(dname, dx, dy);
 }
-function keyPressed() {
-    if (keyCode ==13) {
-      if(usernameText.elt === document.activeElement ){
-          console.log("YES");
-          gameStart();
-      }else{
-          console.log("KEyprs");
-      }
+
+
+
+class Apple{
+    constructor(){
+        this.x;
+        this.y;
+        this.nr;
     }
-  }
+    show(){
+        fill(255,0,0);
+        ellipse(this.x, this.y, 20, 20);
+        fill(0);
+        text(this.nr,this.x,this.y+5);
+        this.hit();
+    }
+    hit(){
+        if(userX > this.x-30 && userX < this.x +30 && userY > this.y-30 && userY < this.y+30){
+            userMass++;
+            socket.emit('move apple', {nr: this.nr});
+            console.log("move apple", this.nr);
+        }
+    }
+}
+
+function keyPressed() {
+    if (keyCode == 13) {
+        if (usernameText.elt === document.activeElement) {
+            console.log("YES");
+            gameStart();
+        } else {
+            console.log("KEyprs");
+        }
+    }
+}
--- server.js
+++ server.js
@@ -7,40 +7,91 @@ const io = require('socket.io')(Server)
 
 //SPILLTING:
 let serverPlayers = {};
+let history = [getTime() + ": Server started"];
+let connectCounter = 0;
 
-
-
-
+let apples = [];
+let nrApples = 100;
+for(i=0;i<nrApples;i++){
+   var apple =  {nr: i, x:Math.random()*2000,y:Math.random()*2000};
+    apples.push(apple);
+}
 
 
 
 
 //Server.listen(PORT, () => console.log('Game server running on:', PORT))
 Server.listen(PORT, () => console.log("\x1b[33m%s\x1b[0m", "Server started at port " + "\x1b[31m" + PORT));
-
+console.log(apples.length + " apples spawned");
+addHistory(apples.length + " apples spawned");
 
 io.on('connection', function (socket) {
-   
-    console.log('a user connected', socket.id);
+    console.log('\x1b[32ma user connected', socket.id, "\x1b[37m total connections: ", connectCounter);
+    io.emit('history', { history });
+    io.emit('total players', { connectCounter });
+
+
     socket.on('disconnect', function () {
-        
-        console.log('user disconnected', socket.id);
+        console.log('\x1b[31muser disconnected', socket.id, "\x1b[37m total connections: ", connectCounter);
+        if (typeof serverPlayers[socket.id] != "undefined") {
+            addHistory(serverPlayers[socket.id].name + " left the game.");
+            connectCounter--;
+        }
+        io.emit('total players', { connectCounter });
         delete serverPlayers[socket.id];
+
     });
+
+
+    socket.on('player joined', function (player) {
+        connectCounter++;
+        addHistory(player + " joined the game.");
+        io.emit('total players', { connectCounter });
+    })
+
     socket.on('send player', function (message) {
-       // console.log(socket.id, " sier ", message);
-
         serverPlayers[socket.id] = message;
-        
-        
-    });
-    socket.on('view data', function(){
-        console.log("\x1b[31m\nSHOWING ALL DATA IN PLAYERS ARRAY: \n","\x1b[37m", serverPlayers);
+        io.emit('update players', serverPlayers);
 
     });
-    socket.on('update players', function(){
-        io.emit('update players', serverPlayers);
+
+    socket.on('view data', function () {
+
+        console.log("\x1b[31m\nSHOWING ALL DATA IN PLAYERS ARRAY: \n", "\x1b[37m", serverPlayers);
+        console.log("\n\n\n\x1b[31m\nSHOWING ALL DATA IN HISTORY ARRAY: \n", "\x1b[37m", history);
+        console.log("\n\n\n\x1b[31m\nSHOWING ALL APPLES IN APPLES ARRAY: \n", "\x1b[37m", apples);
+
     });
-    
+
+    socket.on('get apples', function(){
+        io.emit('apples',{apples});
+    });
+
+    socket.on('move apple',function(numr){
+        if(numr.nr){
+        var nr = numr.nr;
+
+        apples[nr].x = Math.random()*1000;
+        apples[nr].y = Math.random()*1000;
+        var data = apples[nr];
+        console.log("apple moved ", data);
+        io.emit('move apple',{data} );
+    }else{
+        console.log("error: ", numr.nr);
+    }
+         
+    });
 });
 
+
+function addHistory(text) {
+    history.unshift(getTime() + ": " + text);
+    io.emit('history', { history });
+}
+
+function getTime() {
+    var d = new Date();
+    var n = (d.getHours() < 10 ? '0' : '') + d.getHours() + ":" + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() + ":" + (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
+    return n;
+
+}
